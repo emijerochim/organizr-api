fdc1f3c5dc5a18e36f80aba1a1e4c3794e656b55
emijerochim <emijerochim@gmail.com>
2023-02-12 23:33:49 -0300

    users gent endpoint

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,5 +1,5 @@
 const verifyToken = (req, res, next) => {
-  const bearerHeader = req.headers["authorization"];
+  const bearerHeader = req.headers["Authorization"];
   if (typeof bearerHeader !== "undefined") {
     const bearer = bearerHeader.split(" ");
     const bearerToken = bearer[1];
