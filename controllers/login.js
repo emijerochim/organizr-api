f71d6c5f4a0eece4d351964a69500041d0aa959e
emijerochim <emijerochim@gmail.com>
2023-02-18 17:22:49 -0300

    bcrypt

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const handleLogin = async (req, res) => {
   const { username, password } = req.body;
